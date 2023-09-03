21796582221199adbf1827ba53e0737364dfff73
Neeraj Kumar <neeraj.dsu@gmail.com>
2023-09-03 16:48:44 +0500

    Next and Previous Page

--- my-app/src/Components/News.js
+++ my-app/src/Components/News.js
@@ -6,21 +6,55 @@ class News extends Component {
         console.log("This is a constructor component");
         this.state = {
             articles: [], // Initialize as an empty array
-            loading: true
+            loading: true,
+            page: 1,
+
         };
     }
 
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=7fcd230259fb4e45a2697a4db2ad3987";
+        let url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=7fcd230259fb4e45a2697a4db2ad3987&pageSize=20";
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
-        this.setState({ articles: parsedData.articles });
+        this.setState({
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults
+        });
 
-        // console.error("Error fetching data:", error);
 
     }
 
+    handlePrevPage = async () => {
+        console.log('Previous');
+
+        let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=7fcd230259fb4e45a2697a4db2ad3987&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+    }
+
+    handleNextPage = async () => {
+        console.log('Next');
+        if (this.state.page > Math.ceil(this.state.totalResults / 20)) {
+            return;
+        } else {
+            let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=7fcd230259fb4e45a2697a4db2ad3987&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            console.log(parsedData);
+
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+        }
+    }
 
     render() {
         return (
@@ -37,6 +71,11 @@ class News extends Component {
                             />
                         </div>
                     })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button type="button" disabled={this.state.page <= 1} className="btn btn-dark " onClick={this.handlePrevPage}>&larr; Previous Page</button>
+                    <button type="button" className="btn btn-dark " onClick={this.handleNextPage}>Next Page &rarr;</button>
+
                 </div>
             </div>
         );
