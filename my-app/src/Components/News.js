4d66d30a7221b3c8725e788a828f4bac45324aa3
Neeraj Kumar <neeraj.dsu@gmail.com>
2023-09-06 17:36:15 +0500

    Category Wise News Fetched

--- my-app/src/Components/News.js
+++ my-app/src/Components/News.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 class News extends Component {
+    static defaultTypes = {
+        country: 'pak',
+        pageSize: 8,
+        category: 'sports'
+
+    };
+
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string
+    };
+
     constructor() {
         super();
         console.log("This is a constructor component");
@@ -9,12 +24,11 @@ class News extends Component {
             articles: [], // Initialize as an empty array
             loading: false,
             page: 1,
-
         };
     }
 
     async componentDidMount() {
-        let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=7fcd230259fb4e45a2697a4db2ad3987&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7fcd230259fb4e45a2697a4db2ad3987&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
@@ -22,16 +36,13 @@ class News extends Component {
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false
-
         });
-
-
     }
 
     handlePrevPage = async () => {
         console.log('Previous');
 
-        let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=7fcd230259fb4e45a2697a4db2ad3987&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7fcd230259fb4e45a2697a4db2ad3987&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -49,7 +60,7 @@ class News extends Component {
         console.log('Next');
 
         if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-            let url = `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=7fcd230259fb4e45a2697a4db2ad3987&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7fcd230259fb4e45a2697a4db2ad3987&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
             this.setState({ loading: true })
             let data = await fetch(url);
             let parsedData = await data.json();
@@ -67,21 +78,30 @@ class News extends Component {
         return (
             <div className="container my-4" >
                 <h1>NewsApp - Top Headline</h1>
+
                 {/* If loading is true, show spinner */}
                 {this.state.loading && <Spinner />}
                 <div className="row">
+
                     {/* if loading is false, then show news */}
                     {!this.state.loading && this.state.articles.map((element) => {
-                        return <div className="col-md-4" key={element.url}>
-                            <NewsItem
-                                title={element.title}
-                                description={element.description}
-                                imageUrl={element.urlToImage}
-                                newsUrl={element.url}
-                            />
-                        </div>
+
+                        // if news item is removed, dont show
+                        if (element.title !== '[Removed]') {
+                            return <div className="col-md-4" key={element.url}>
+                                <NewsItem
+                                    title={element.title}
+                                    description={element.description}
+                                    imageUrl={element.urlToImage}
+                                    newsUrl={element.url}
+                                />
+                            </div>
+                        }
+                        return null;
+
                     })}
                 </div>
+
                 <div className="container d-flex justify-content-between">
                     <button type="button" disabled={this.state.page <= 1} className="btn btn-dark " onClick={this.handlePrevPage}>&larr; Previous Page</button>
                     <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-dark " onClick={this.handleNextPage}>{this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize) ? 'No More Pages' : 'Next Page'}&rarr;</button>
--- my-app/src/Components/NewsItem.js
+++ my-app/src/Components/NewsItem.js
@@ -9,9 +9,10 @@ class NewsItem extends Component {
             <div className="card my-3" style={{ width: "18rem" }}>
                 <img src={!imageUrl ? "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png" : imageUrl} style={{ height: '10rem' }} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">{title.slice(0, 45)}...</h5>
-                    <p className="card-text">{description.slice(0, 88)}...</p>
-                    <a href={newsUrl} target='_blank' className="btn btn-primary">Click for Details</a>
+                    <h5 className="card-title">{title ? title.slice(0, 45) : ''}...</h5>
+                    <p className="card-text">{description ? description.slice(0, 88) : ''}...</p>
+
+                    <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-primary">Click for Details</a>
                 </div>
             </div>
         );
