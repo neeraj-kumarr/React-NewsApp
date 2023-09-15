055bb76c08727b15321dac1d7c1e0a12e1de7605
Neeraj Kumar <neeraj.dsu@gmail.com>
2023-09-15 18:04:43 +0500

    Updates

---
+++ precache-manifest.dde10eb72e3289599b8f15ad4dcbffc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b914ec5c19582f67259a",
+    "url": "/React-NewsPulse/static/css/main.9dbf8a8a.chunk.css"
+  },
+  {
+    "revision": "b914ec5c19582f67259a",
+    "url": "/React-NewsPulse/static/js/main.1add45aa.chunk.js"
+  },
+  {
+    "revision": "988b39a51a2369ef682d",
+    "url": "/React-NewsPulse/static/js/runtime~main.7c5397cf.js"
+  },
+  {
+    "revision": "ddfe7bbe983d5ca28735",
+    "url": "/React-NewsPulse/static/js/2.a727f26c.chunk.js"
+  },
+  {
+    "revision": "03c2247ebef71b42a125",
+    "url": "/React-NewsPulse/static/js/3.133d7e0c.chunk.js"
+  },
+  {
+    "revision": "477e034f9d5f1ff80b9b143a8662f47c",
+    "url": "/React-NewsPulse/static/media/loading.477e034f.gif"
+  },
+  {
+    "revision": "6d365304c71290bacf1a66a36b5df2c4",
+    "url": "/React-NewsPulse/index.html"
+  }
+];
